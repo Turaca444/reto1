0e3a764c55f7998ae608ceb2692413114d5b91a4
Turaca444 <61064291+Turaca444@users.noreply.github.com>
2024-02-08 23:52:35 -0300

    Add files via upload

---
+++ reto1.py
@@ -0,0 +1,6 @@
+nombre = input('Ingrese su Nombre(s): ')
+apellido = input('Ingrese su Apellido(s): ')
+numero_de_telefono = input('Ingrese su Numero de Telefono: ')
+correo_electronico = input('Ingrese de e-mail: ')
+
+print('Hola ' +  nombre + " " + apellido + ' en breve recibirás un Correo a: '+ correo_electronico)
